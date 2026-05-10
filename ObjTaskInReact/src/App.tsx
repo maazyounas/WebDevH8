49606badf60b04c3285accebb6c539771f2bc45a
maaz <maazyounas06@gmail.com>
2026-05-10 14:58:50 +0500

    api calling in react

--- ObjTaskInReact/src/App.tsx
+++ ObjTaskInReact/src/App.tsx
@@ -1,6 +1,6 @@
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
-import Home from "./pages/home";
+import Home from "./pages/Home";
 
 function App() {
 
--- useEffectInReact/src/reactHook/EffectHook.tsx
+++ useEffectInReact/src/reactHook/EffectHook.tsx
@@ -3,18 +3,20 @@ import {  useEffect, useState } from 'react'
 const EffectHook = () => {
 
 const [value, setValue] = useState(0)
+
 const [value2, setValue2] = useState(0)
 
 
 useEffect(() =>{
-    console.log('Effect is running On increment ')
+    console.log('Effect is running in increment  ')
 }, [value])
-
 
   return (
     <div>
       <p>Value: {value}</p>
       <button onClick={() => setValue(value + 1)}>Increment Value</button>
+
+
       <p>Value2: {value2}</p>
       <button onClick={() => setValue2(value2 - 1)}>Decrement Value2</button>
     </div>
